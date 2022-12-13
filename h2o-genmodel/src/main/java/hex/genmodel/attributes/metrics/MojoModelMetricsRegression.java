ee24e4094f11c089a404a9b934eb73f78d632981
syzonyuliia <yuliia.syzon@h2o.ai>
2022-12-13 20:58:56 +0100

    PUBDEV-8708: add deviance2 to the MOJO

--- h2o-genmodel/src/main/java/hex/genmodel/attributes/metrics/MojoModelMetricsRegression.java
+++ h2o-genmodel/src/main/java/hex/genmodel/attributes/metrics/MojoModelMetricsRegression.java
@@ -4,6 +4,7 @@ import hex.genmodel.attributes.SerializedName;
 
 public class MojoModelMetricsRegression extends MojoModelMetricsSupervised {
   public double _mean_residual_deviance;
+  public double _mean_residual_deviance_2;
   @SerializedName("rmsle")
   public double _root_mean_squared_log_error;
 }
